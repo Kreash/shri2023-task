6b4e706093d7b62c795f87c95b1e01002c6e8729
Kreash <anton.kreash@gmail.com>
2023-07-05 02:58:20 +0300

    Сборка проекта через esbuild

---
+++ esbuild.mjs
@@ -0,0 +1,27 @@
+﻿import * as esbuild from 'esbuild';
+import fs from 'fs';
+
+function readFile(file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+await esbuild.build({
+  entryPoints: ['src/scripts/main.jsx', 'src/styles/style.css', 'src/styles/fonts.css'],
+  bundle: true,
+  minify: true,
+  loader: { '.png': 'file', '.svg': 'file', '.woff2': 'file' },
+  outdir: '/dist',
+});
+
+
+
+const indexHtml = await readFile('src/index.html');
+fs.writeFileSync('dist/index.html', indexHtml);
