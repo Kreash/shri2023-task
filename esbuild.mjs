e7edb0b980b9db1f44072bf017a654d32558ad0f
Kreash <anton.kreash@gmail.com>
2023-07-05 03:33:12 +0300

    fix build 9

--- esbuild.mjs
+++ esbuild.mjs
@@ -18,7 +18,7 @@ await esbuild.build({
   bundle: true,
   minify: true,
   loader: { '.png': 'file', '.svg': 'file', '.woff2': 'file' },
-  outdir: '/dist',
+  outdir: '/dist/shri2023-task',
 });
 
 
